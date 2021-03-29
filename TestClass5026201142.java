c04e244342537f16ab1a6728443b99a41bae7b8d
farreladitya <81145903+farreladitya@users.noreply.github.com>
2021-03-29 09:35:22 +0700

    Add files via upload

---
+++ TestClass5026201142.java
@@ -0,0 +1,8 @@
+public class TestClass5026201142 {
+    public static void main(String[] args) {
+        ComputeMethods5026201142 result = new ComputeMethods5026201142();
+        result.fToC(212);
+        result.hypotenuse(5,12);
+        result.roll();
+    }
+}
